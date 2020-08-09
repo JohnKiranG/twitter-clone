10e3484ad8a305d41c48605ff64c455e8b6b3dda
JohnKiranG <jkiran989@gmail.com>
2020-08-09 15:05:33 +0000

    Updates

---
+++ precache-manifest.1101d6431c5fa7ea0877462386f1cc36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa62500a0d042db568d966e5b0b0a5d",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "ef3968c90826474e32e6",
+    "url": "/twitter-clone/static/css/main.73c25be4.chunk.css"
+  },
+  {
+    "revision": "b7078e8776075397e7a3",
+    "url": "/twitter-clone/static/js/2.6e4b4d9c.chunk.js"
+  },
+  {
+    "revision": "a8de4128b679d6159aad8871b5569c6d",
+    "url": "/twitter-clone/static/js/2.6e4b4d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef3968c90826474e32e6",
+    "url": "/twitter-clone/static/js/main.5789bae2.chunk.js"
+  },
+  {
+    "revision": "4ea1c8ca6c4e7890228d",
+    "url": "/twitter-clone/static/js/runtime-main.a74d6e56.js"
+  }
+]);
